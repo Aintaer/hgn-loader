31a6d1b3fab4f0bf7055ae48173593cae150cbc8
Yuriy Nemtsov <nemtsov@gmail.com>
2016-05-25 17:05:59 -0400

    Add support for deep partials and helpers.

--- src/index.js
+++ src/index.js
@@ -13,24 +13,30 @@ function hgn(source) {
   let partialNames = {};
 
   // using object map to eliminate duplicates
-  for (let p in partials) {
-    let name = partials[p].name;
+  (function recurse(partialNames, partials) {
+    for (let p in partials) {
+      let name = partials[p].name;
 
-    // skip if not a path
-    if (!isPath.test(name)) { continue; }
+      // skip if not a path
+      if (!isPath.test(name)) { continue; }
 
-    // skip if it's not a request
-    if (!loaderUtils.isUrlRequest(name, root)) { continue; }
+      // skip if it's not a request
+      if (!loaderUtils.isUrlRequest(name, root)) { continue; }
 
-    // definite names (no prefixed metadata) are prefixed
-    partialNames[name] = isDefinite.test(name) ?
-      `${hgn.prefix || ''}${name}` :
-      name;
+      // definite names (no prefixed metadata) are prefixed
+      partialNames[name] = isDefinite.test(name) ?
+        `${hgn.prefix || ''}${name}` :
+        name;
 
-    if (!isDefinite.test(partialNames[name])) {
-      partialNames[name] = loaderUtils.urlToRequest(name, root);
+      if (!isDefinite.test(partialNames[name])) {
+        partialNames[name] = loaderUtils.urlToRequest(name, root);
+      }
+
+      if (partials[p].partials) {
+        recurse(partialNames, partials[p].partials);
+      }
     }
-  }
+  })(partialNames, partials);
 
   const loaders = this.loaders.slice(this.loaderIndex).map(obj => obj.request);
 
@@ -39,7 +45,9 @@ function hgn(source) {
   return `var Hogan = require("hogan.js"),
   preloads = {${toLoad.join(',')}},
   template = new Hogan.Template(${Hogan.compile(source, { asString: true })}, ${JSON.stringify(text)}, Hogan);
+  function extend(target, source) { return Object.keys(source).reduce(function(t, p) { t[p] = source[p]; return t; }, target); }
   template.ri = function(context, partials, indent) {
+    if (Hogan.helpers) context.unshift(Hogan.helpers);
     return this.r(context, extend(preloads, partials), indent);
   };
   module.exports = template.render.bind(template);
